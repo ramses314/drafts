17560aa3e402ea3b8fd52ae8d36a8d76676ef686
ramses <forwork31415>
2022-08-09 12:10:14 +0700

    test

--- network/mysite/account/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Profile
-
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'date_birth', 'photo']
